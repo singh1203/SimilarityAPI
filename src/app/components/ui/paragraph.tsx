084e5af2175d0808bdf026ef3dad6dc763add11e
Saurabh Kumar Singh <singh1203.ss@gmail.com>
2023-10-23 22:15:02 +0530

    Done some with paragraph.tsx
    Signed-off-by: Saurabh Kumar Singh <singh1203.ss@gmail.com>

--- src/app/components/ui/paragraph.tsx
+++ src/app/components/ui/paragraph.tsx
@@ -1,14 +1,30 @@
-import { FC }  from 'react';
-import { cva} from 'class-variance-authority';
+import { FC, forwardRef, HTMLAttributes }  from 'react';
+import { VariantProps, cva} from 'class-variance-authority';
 
-interface ParagraphProps {}
 
 const paragraphvariants = cva(
-    'max-w-prose text-slate-700 dark:text-slate-300 mb-2 text-centre'
+    'max-w-prose text-slate-700 dark:text-slate-300 mb-2 text-centre',
+    {
+        variants: {
+            size: {
+                default: 'text-base sm:text-lg',
+                sm: 'text-sm sm:text-base',
+            }
+        },
+        defaultVariants: {
+            size: 'default',
+        },
+    }
 )
 
-const Paragraph: FC<ParagraphProps> = ({}) => {
-    return <div> Paragraph</div>
-}
+interface ParagraphProps 
+extends HTMLAttributes<HTMLParagraphElement>, 
+VariantProps<typeof paragraphvariants> {}
+
+const Paragraph = forwardRef<HTMLParagraphElement, ParagraphProps> (
+    ({ className, size, children, ...props }, ref) => {
+        return <p ref={ref} {...props} className={}></p>
+    }
+)
 
 export default Paragraph;
