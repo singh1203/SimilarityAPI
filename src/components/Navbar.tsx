0c083f3a4457f47d50e705e2e005b0e6a6014db1
Saurabh Kumar Singh <singh1203.ss@gmail.com>
2023-11-11 23:06:28 +0530

    Added Button.tsx component
    Signed-off-by: Saurabh Kumar Singh <singh1203.ss@gmail.com>

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,8 @@
+import { Sign } from "crypto";
 import { getServerSession } from "next-auth";
 import Link from "next/link";
 import { FC } from "react";
+import { buttonVariants } from "./ui/Button";
 
 interface NavbarProps {}
 
@@ -9,7 +11,29 @@ const Navbar = async({}) => {
     return (
         <div className="fixed backdrop-blur-sm bg-white/75 dark:bg-slate-900 z-50 top-0 left-0 right-0 h-20 border-b border-slate-300 dark:border-slate-700 shadow-sm flex items-center justify-between">
             <div className="container max-w-7xl mx-auto w-full flex justify-between items-center">
-                <Link href="/"></Link>
+                <Link href="/" className={buttonVariants({variant: 'link'})}>
+                    Text Similarity 1.0
+                </Link>
+
+                <div className="md:hidden">
+                    <ThemeToggle />
+                </div>
+
+                <div className="hidden md:flex gap-4">
+                    <ThemeToggle />
+                    <Link href="/documentation" className={buttonVariants({variants: 'ghost'})}>
+                        Documentation
+                    </Link>
+
+                    { session ? (
+                        <>
+                            <Link className={buttonVariants({variants: 'ghost'})} href="/dashboard">
+                                Dashboard
+                            </Link>
+                            <SignOutButton />
+                        </>
+                    ) : <SignInButton />}
+                </div>
             </div>
         </div>
     );
