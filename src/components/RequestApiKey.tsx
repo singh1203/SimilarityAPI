d5a077692bed88b26c2a2c44315902e914ae56ac
Saurabh Kumar Singh <singh1203.ss@gmail.com>
2023-12-24 22:13:21 +0530

    Added CopyButton & Input UI
    Signed-off-by: Saurabh Kumar Singh <singh1203.ss@gmail.com>

--- src/components/RequestApiKey.tsx
+++ src/components/RequestApiKey.tsx
@@ -3,13 +3,18 @@
 import { createApiKey } from '@/helpers/create-api-key'
 import { FC, FormEvent, useState } from 'react'
 import { toast } from '@/ui/Toast'
+import { Key } from 'lucide-react'
+import LargeHeading from '@/ui/LargeHeading'
+import Paragraph from '@/ui/Paragraph'
+import CopyButton from '@/components/CopyButton'
+import { Input } from '@/ui/Input'
 
 
 const RequestApiKey: FC = () => {
     const [isCreating, setIsCreating] = useState<boolean>(false)
     const [apiKey, setApiKey] = useState<string | null>(null)
 
-    const createApiKey = async (e: FormEvent<HTMLFormElement>) => {
+    const createNewApiKey = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault()
 
         setIsCreating(true)
@@ -37,7 +42,24 @@ const RequestApiKey: FC = () => {
         }
     }
 
-    return <div>RequestApiKey</div>
+    return <div  className='container md:max-w-2xl'>
+        <div className='flex flex-col gap-6 items-center'>
+            <Key className='mx-auto h-12 w-12 text-gray-400' />
+            <LargeHeading >Request your API Key</LargeHeading>
+            <Paragraph>You haven&apos;t requested an API key yet.</Paragraph>
+        </div>
+        <form
+            onSubmit={createNewApiKey}
+            className='mt-6 sm:flex sm:items-center'
+            action='#'>
+                <div className='relative rounded-md shadow-dm sm:min-w-0 sm:flex-1'>
+                    {apiKey ? (
+                        <CopyButton className='absolute inset-y-0 right-0 animate-in fade-in duration-300' valueToCopy={apiKey}/>
+                        <Input />
+                    ) : null}
+                </div>
+        </form>
+    </div>
 }
 
 export default RequestApiKey
