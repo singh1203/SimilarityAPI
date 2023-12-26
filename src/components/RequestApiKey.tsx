391e70fec6343e0e99ddb2de0323a857ca4bfeca
Saurabh Kumar Singh <singh1203.ss@gmail.com>
2023-12-26 22:20:28 +0530

    Started with ApiDashboard component and done with RequestApiKey
    Signed-off-by: Saurabh Kumar Singh <singh1203.ss@gmail.com>

--- src/components/RequestApiKey.tsx
+++ src/components/RequestApiKey.tsx
@@ -8,6 +8,7 @@ import LargeHeading from '@/ui/LargeHeading'
 import Paragraph from '@/ui/Paragraph'
 import CopyButton from '@/components/CopyButton'
 import { Input } from '@/ui/Input'
+import Button from '@/ui/Button'
 
 
 const RequestApiKey: FC = () => {
@@ -42,24 +43,29 @@ const RequestApiKey: FC = () => {
         }
     }
 
-    return <div  className='container md:max-w-2xl'>
-        <div className='flex flex-col gap-6 items-center'>
-            <Key className='mx-auto h-12 w-12 text-gray-400' />
-            <LargeHeading >Request your API Key</LargeHeading>
-            <Paragraph>You haven&apos;t requested an API key yet.</Paragraph>
+    return (
+        <div  className='container md:max-w-2xl'>
+            <div className='flex flex-col gap-6 items-center'>
+                <Key className='mx-auto h-12 w-12 text-gray-400' />
+                <LargeHeading >Request your API Key</LargeHeading>
+                <Paragraph>You haven&apos;t requested an API key yet.</Paragraph>
+            </div>
+            <form
+                onSubmit={createNewApiKey}
+                className='mt-6 sm:flex sm:items-center'
+                action='#'>
+                    <div className='relative rounded-md shadow-dm sm:min-w-0 sm:flex-1'>
+                        { apiKey ? (
+                            <CopyButton className='absolute inset-y-0 right-0 animate-in fade-in duration-300' valueToCopy={apiKey}/>
+                        ) : null }
+                        <Input readOnly value={apiKey ?? ''} placeholder='Request an API Key to display it here!'/>
+                    </div>
+                    <div className='mt-3 flex justify-center sm:mt-0 sm:ml-4 sm:flex-shrink-0'>
+                        <Button disabled={!!apiKey} isLoading={isCreating}>Request key</Button>
+                    </div>
+            </form>
         </div>
-        <form
-            onSubmit={createNewApiKey}
-            className='mt-6 sm:flex sm:items-center'
-            action='#'>
-                <div className='relative rounded-md shadow-dm sm:min-w-0 sm:flex-1'>
-                    {apiKey ? (
-                        <CopyButton className='absolute inset-y-0 right-0 animate-in fade-in duration-300' valueToCopy={apiKey}/>
-                        <Input />
-                    ) : null}
-                </div>
-        </form>
-    </div>
+    ) 
 }
 
 export default RequestApiKey
